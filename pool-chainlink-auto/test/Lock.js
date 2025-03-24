8ceb61ba7caaffb49cc5915b0e222f07a50ef8d0
ernnies <ernestnnamdie@gmail.com>
2025-03-24 10:03:48 +0100

    Restricted the AccessControl

--- pool-chainlink-auto/test/Lock.js
+++ pool-chainlink-auto/test/Lock.js
@@ -108,19 +108,5 @@ describe("Lock", function () {
       });
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
+    
 });

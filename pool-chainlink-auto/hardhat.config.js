b398fab0ec1012d9af40d271d070205440096b40
ernnies <ernestnnamdie@gmail.com>
2025-03-24 10:23:04 +0100

    Restricted the AccessControl

--- pool-chainlink-auto/hardhat.config.js
+++ pool-chainlink-auto/hardhat.config.js
@@ -1,26 +1,3 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 let secrets = require("./secrets");
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.20",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
-  },ey],
-    },
-  },
-  etherscan: {
-    //apiKey: "YJJJCUX1H4EGWT6D5URIF1QNGXSN9EDA14",
-    apiKey: "F22ID1GZ8Y445Z5NY1AN9P9F2UWGBZQ59Y",
-  },
-};
